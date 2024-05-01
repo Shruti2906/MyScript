87b8bec13b194c798c13fb7d151d8ebbe9e572e6
Shruti <65824891+Shruti2906@users.noreply.github.com>
2024-05-01 16:36:10 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -6,4 +6,6 @@ console.log('my script loaded');
 //     script.src = src;
 //     document.body.appendChild(script);
 // }
-// loadscript('http://127.0.0.1:5500/myscript.js');
+// loadscript('http://127.0.0.1:5500/myscript.js'); 
+//cdn link:=  https://cdn.jsdelivr.net/gh/Shruti2906/MyScript@main/myscript.js
+//loadscript('https://cdn.jsdelivr.net/gh/Shruti2906/MyScript@main/myscript.js'); 
