602e767d8c1403d3c15223158ea79edcc33cfef5
Shruti <65824891+Shruti2906@users.noreply.github.com>
2024-05-01 16:26:01 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,9 @@
+alert('my script loaded');
+console.log('my script loaded');
+
+// function loadscript(src){
+//     let script = document.createElement('script');
+//     script.src = src;
+//     document.body.appendChild(script);
+// }
+// loadscript('http://127.0.0.1:5500/myscript.js');
